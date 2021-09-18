16e4e1390e5a694adc35288e8d5fabd914503a2f
Kurisu Brooks <kurisubrooks@gmail.com>
2021-09-19 00:53:17 +1000

    Add Next Services Bar

--- src/components/NextServicesBar.js
+++ src/components/NextServicesBar.js
@@ -1,5 +1,39 @@
-export const NextServicesBar = () => {
-  return <div>
-    {/* uwu */}
+import React, { useContext } from 'react';
+import { DepartureTimeCountdown, truncateStationName } from '../util';
+import State from '../state';
+import '../assets/styles/NextServicesBar.scss';
+
+export const NextServicesBar = ({ services }) => {
+  const { theme } = useContext(State);
+  const servicesLimit = 2;
+
+  return <div className={`nextServices ${theme}`}>
+    <div className="rows">
+      <div className="row">
+        <div className="cell">Next Services</div>
+        <div className="cell">Platform</div>
+        <div className="cell">Departs</div>
+      </div>
+      {services.slice(1, servicesLimit + 1).map(i =>
+        <NextServiceItem service={i} key={i.id} />
+      )}
+    </div>
   </div>;
+};
+
+const NextServiceItem = ({ service }) => {
+  let [lineTo, lineVia] = service.destination
+    ? service.destination.includes('via')
+      ? service.destination.split(' via ')
+      : service.destination.split()
+    : [null, null];
+
+  return <>
+    <div className="row">
+      <div className="cell">{truncateStationName(lineTo)}</div>
+      <div className="cell">{service.platform.value}</div>
+      <div className="cell">{DepartureTimeCountdown(service.departs)}</div>
+    </div>
+    <div className="row via">{lineVia && 'via ' + lineVia}</div>
+  </>;
 };
--- src/views/NextTrainView.js
+++ src/views/NextTrainView.js
@@ -1,11 +1,14 @@
+import React, { useContext } from 'react';
 import { ServiceView } from './ServiceView';
 import { ServiceBar } from '../components/ServiceBar';
 import { NextServicesBar } from '../components/NextServicesBar';
+import State from '../state';
 
 export const NextTrainView = ({ services, departureTimer }) => {
+  const { isLandscape } = useContext(State);
   return <>
     <ServiceBar service={services[0]} />
     <ServiceView services={services} stops={services[0]?.stops} departure={departureTimer} />
-    <NextServicesBar />
+    {!isLandscape && <NextServicesBar services={services} />}
   </>;
 };
